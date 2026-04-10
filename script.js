9d48eebcbf317eea87cf26ec9d2842e826bfa023
clearpather <mewantbounty@gmail.com>
2026-04-10 07:02:49 +0000

    nvm claude

--- script.js
+++ script.js
@@ -34,21 +34,16 @@ function getDaySuffix(day) {
 function time() {
     let date = new Date();
     let monthName = months[date.getMonth()];
-    let day = getDaySuffix(date.getDate());
+    let dayNum = date.getDate();
+    let day = getDaySuffix(dayNum);
     let year = date.getFullYear();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
     
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
     
     return monthName + " " + day + ", " + year + " " + hours + ":" + minutes + ":" + seconds;
 }
